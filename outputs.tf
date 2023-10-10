e8be968d56701f851e835ccaf5143c85d9104581
Alex Poliukh <alex.poliukh@gmail.com>
2023-10-10 10:12:25 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@ output "vpc_id" {
   value = module.Networking.vpc_id
 }
 
-output "private_subnets" {
-  value = module.Networking.private_subnets
+output "public_subnets" {
+  value = module.Networking.public_subnets
 }
 
-output "intra_subnets" {
-  value = module.Networking.intra_subnets
+output "private_subnets" {
+  value = module.Networking.private_subnets
 }
